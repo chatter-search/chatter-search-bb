5acff41d375c70cdfeac35c6a7889d421b0770c7
dmi3y <lapshukov@gmail.com>
2016-03-04 02:52:26 -0800

    cleanup

--- src/script/bites/templates.js
+++
@@ -1,6 +0,0 @@
-define(function(require) {
-	var Handlebars = require("handlebars.runtime");
-	require("__templates");
-	Handlebars.partials = Handlebars.templates;
-	return Handlebars.templates;
-});
