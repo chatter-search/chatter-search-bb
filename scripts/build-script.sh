71a43fe66b83679ee5eacaf3c0302fab90854ac2
dmi3y <lapshukov@gmail.com>
2016-03-04 02:50:46 -0800

    build script

--- scripts/build-script.sh
+++ scripts/build-script.sh
@@ -8,7 +8,6 @@ jquery/dist/jquery.js,\
 underscore/underscore.js,\
 backbone/backbone.js} \
 build/script
-# cp bower_components/requirejs-plugins/src/json.js build/script/requirejs-json.js
 cp node_modules/handlebars/dist/handlebars.runtime.js build/script
 
 cp -r src/script/* build/script
