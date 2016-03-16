c0a8b4271cbf819eab48c39561aec03bdff44964
dmi3y <lapshukov@gmail.com>
2016-03-15 23:27:19 -0700

    Hope it is only formatting...

--- src/script/actions/main.js
+++ src/script/actions/main.js
@@ -1,19 +1,19 @@
-define(function(require) {
+/* global define */
+define(function (require) {
+  var MainView = require('views/MainView')
+  var UserShowModel = require('models/UserShowModel')
+  var UserTimelineCollection = require('collections/UserTimelineCollection')
+  var $ = require('jquery')
 
- 	var	MainView = require("views/MainView");
- 	var	UserShowModel = require("models/UserShowModel");
- 	var UserTimelineCollection = require("collections/UserTimelineCollection");
- 	var $ = require("jquery");
+  return function () {
+    var userShowModel = new UserShowModel()
+    var userTimelineCollection = new UserTimelineCollection()
+    var mainView = new MainView({
+      model: userShowModel,
+      collection: userTimelineCollection
+    })
 
- 	return function() {
- 		var userShowModel = new UserShowModel();
- 		var userTimelineCollection = new UserTimelineCollection();
- 		var mainView = new MainView({
- 			model: userShowModel,
- 			collection: userTimelineCollection
- 		});
-
- 		mainView.render(userShowModel);
- 		$("#user-show").prepend(mainView.$el);
-	};
-});
+    mainView.render(userShowModel)
+    $('#user-show').prepend(mainView.$el)
+  }
+})
