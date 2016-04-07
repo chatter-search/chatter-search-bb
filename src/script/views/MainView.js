d9b4bc4b7a6b4919de34627ae87bf30f6c955cf4
dmi3y <lapshukov@gmail.com>
2016-04-07 12:09:28 -0700

    formatting

--- src/script/views/MainView.js
+++ src/script/views/MainView.js
@@ -2,68 +2,70 @@
  * User Show View
  */
 
-define(function(require) {
-	"use strict";
+ /*global define*/
 
-	var Backbone = require("backbone");
-	var $ = require("jquery");
-	var _ = require("underscore");
+define(function (require) {
+  'use strict'
 
-	var	UserTimelineView = require("views/UserTimelineView");
-	var templates = require("spices/templates");
-	var showTemplate = templates.userShow;
+  var Backbone = require('backbone')
+  var $ = require('jquery')
+  var _ = require('underscore')
 
-	return Backbone.View.extend({
-		template: showTemplate,
-		events: {
-			"click button": "searchButton",
-			"submit form": "submitForm"
-		},
-		initialize: function(opt) {
-			this.model.bind("change", this.render, this);
-		},
-		render: function(model) {
-			var data = model.toJSON();
-			var rendered = this.template(data);
-			this.$el.html(rendered);
-			return this;
-		},
-		renderUserTimeline: function() {
-			this.$(".twitter").removeClass("loading");
-	 		this.userTimelineView = new UserTimelineView({
-	 			collection: this.collection
-	 		});
-	 		this.userTimelineView.render();
-		},
-		searchButton: function() {
-			this.$(".search").toggleClass("close");
-			this.$(".input").toggleClass("square");
+  var UserTimelineView = require('views/UserTimelineView')
+  var templates = require('spices/templates')
+  var showTemplate = templates.userShow
 
-			if (this.$(".search").hasClass("close")) {
-				this.$("input").focus();
-				this.$("#content").addClass("expanded");
-			} else {
-				this.$("#content").removeClass("expanded");
-				this.$("input").blur();
-			}
-		},
-		submitForm: function(ev) {
-			ev.preventDefault();
-			this.$(".twitter").addClass("loading");
-			var query = $(ev.target).serializeObject();
-			this.fetchUserData(query);
-		},
-		fetchUserData: function(query) {
-			var success = _.bind(this.renderUserTimeline, this);
-			this.model.fetch({	
-				data: query,
-				reset: true
-			});
-			this.collection.fetch({	
-				data: query,
-				reset: true,
-				success: success
-			});
-		}
-	});
-});
+  return Backbone.View.extend({
+    template: showTemplate,
+    events: {
+      'click button': 'searchButton',
+      'submit form': 'submitForm'
+    },
+    initialize: function (opt) {
+      this.model.bind('change', this.render, this)
+    },
+    render: function (model) {
+      var data = model.toJSON()
+      var rendered = this.template(data)
+      this.$el.html(rendered)
+      return this
+    },
+    renderUserTimeline: function () {
+      this.$('.twitter').removeClass('loading')
+      this.userTimelineView = new UserTimelineView({
+        collection: this.collection
+      })
+      this.userTimelineView.render()
+    },
+    searchButton: function () {
+      this.$('.search').toggleClass('close')
+      this.$('.input').toggleClass('square')
+
+      if (this.$('.search').hasClass('close')) {
+        this.$('input').focus()
+        this.$('#content').addClass('expanded')
+      } else {
+        this.$('#content').removeClass('expanded')
+        this.$('input').blur()
+      }
+    },
+    submitForm: function (ev) {
+      ev.preventDefault()
+      this.$('.twitter').addClass('loading')
+      var query = $(ev.target).serializeObject()
+      this.fetchUserData(query)
+    },
+    fetchUserData: function (query) {
+      var success = _.bind(this.renderUserTimeline, this)
+      this.model.fetch({
+        data: query,
+        reset: true
+      })
+      this.collection.fetch({
+        data: query,
+        reset: true,
+        success: success
+      })
+    }
+  })
+})
