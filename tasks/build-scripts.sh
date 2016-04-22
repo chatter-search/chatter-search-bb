82e79b00093705b10f990218411166c72cb39684
dmi3y <lapshukov@gmail.com>
2016-04-22 15:28:16 -0700

    fixing

--- tasks/build-scripts.sh
+++ tasks/build-scripts.sh
@@ -3,7 +3,7 @@
 cp node_modules/{\
 jquery/dist/jquery.js,\
 underscore/underscore.js,\
-backbone/backbone.js, \
+backbone/backbone.js,\
 handlebars/dist/handlebars.runtime.js,\
 requirejs/require.js} \
 build/scripts
