50fad67e0a77640cfacf3efe65e8529ad8a61d87
dmi3y <lapshukov@gmail.com>
2016-05-02 23:39:27 -0700

    reorder commands, trying to catch race condition

--- tasks/build.sh
+++ tasks/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 npm run build-index & \
-npm run copy-dependencies & \
-npm run compile-scripts & \
-npm run compile-templates & \
 npm run compile-styles &\
+npm run compile-scripts & \
+npm run copy-dependencies & \
+npm run compile-templates & \
 
 wait
